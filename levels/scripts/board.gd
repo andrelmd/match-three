a58f9046936977abf296632df7b0b732ee1f991b
André Luís Maldonado Daniel <andre_lmd2@hotmail.com>
2025-03-24 21:32:34 -0300

    refactors board to use simple node2D - simplifies logic

--- levels/scripts/board.gd
+++ levels/scripts/board.gd
@@ -1,220 +1,217 @@
-extends TileMapLayer
+extends Node2D
 
-@export var board_size = Vector2i(8, 8)
+@export var width: int = 8
+@export var height: int = 8
+@export var piece_size = Vector2i(16, 16)
+@export var board_pieces: Array = []
+
 @export var possible_piences: Array[PackedScene]
+@export var destroy_match_timer: Timer
+@export var slide_down_timer: Timer
+@export var refill_timer: Timer
 
-var pieces_on_board: Array = []
-var selected_piece: Piece
-var tween: Tween
-var can_move: bool = true
+var selected_piece: Piece = null
 
-func generate_cells() -> Array[Vector2i]:
-	var cells: Array[Vector2i]
+func _ready():
+	randomize()
+	destroy_match_timer.timeout.connect(_on_destroy_match_timer_timeout)
+	slide_down_timer.timeout.connect(_on_slide_down_timer_timeout)
+	refill_timer.timeout.connect(_on_refill_timer_timeout)
+	_initialize_board_array()	
+	_initialize_board()
 
-	for x in range(board_size.x):
-		for y in range(board_size.y):
-			cells.append(Vector2i(x, y))
 
-	return cells
+func _grid_to_pixel(board_position: Vector2i) -> Vector2:
+	return Vector2(board_position * piece_size)
 
-func generate_2d_array():
-	var array = []
 
-	for x in range(board_size.x - 2):
-		array.append([])
-		for y in range(board_size.y - 2):
-			array[x].append(null)
+func _pixe_to_grid(pixel_position: Vector2) -> Vector2i:
+	return Vector2i(pixel_position) / piece_size
 
-	return array
 
-func randomize_piece() -> PackedScene:
-	var piece = possible_piences[randi() % possible_piences.size()]
+func _initialize_board_array() -> void:
+	for row in range(width):
+		var vec = []
+		for column in range(height):
+			vec.append(null)
+		board_pieces.append(vec)
+
+
+func _initialize_board() -> void:
+	for row in range(width):
+		for column in range(height):
+			var board_position = Vector2i(row, column)
+			var new_piece = _create_piece()
+			var loop = 0
+			while _check_match(row, column, new_piece.piece_type) and loop < 100:
+				print("piece", new_piece)
+				print("piece type: ", new_piece.piece_type)
+				print("loop: ", loop)
+				new_piece.queue_free()
+				new_piece = _create_piece()
+				loop += 1
+			board_pieces[board_position.x][board_position.y] = new_piece
+			new_piece.move(board_position * piece_size)
+
+
+func _move_piece(from: Vector2i, to: Vector2i) -> void:
+	var from_piece: Piece = board_pieces[from.x][from.y]
+	var to_piece: Piece = board_pieces[to.x][to.y]
+	
+	if (to_piece == null):
+		return
+		
+	if (from_piece == null):
+		return
+	
+	board_pieces[to.x][to.y] = from_piece
+	board_pieces[from.x][from.y] = to_piece
+	
+	from_piece.move(_grid_to_pixel(to))
+	to_piece.move(_grid_to_pixel(from))
+	
+	_find_and_set_matchs()
+
+
+func _create_piece() -> Piece:
+	var piece: Piece = possible_piences[randi() % possible_piences.size()].instantiate()
+	add_child(piece)
+	piece.piece_selected.connect(_on_piece_piece_selected)
 	return piece
 
-func create_piece(cell: Vector2i):
-	var piece: Piece = randomize_piece().instantiate()
-	var array_position = _board_to_array_position(cell)
-	var piece_name = piece.piece_name
-	print("Creating piece: ", piece_name, " at: ", array_position)
-	print("Checking for matches")
-	while (_match_3_at(cell, piece_name)):
-		print("Match 3 found, re-rolling piece")
-		piece.queue_free()
-		piece = randomize_piece().instantiate()
-		piece_name = piece.piece_name
-	
-	# var new_label: Label = Label.new()
-	# new_label.scale = Vector2(0.4, 0.4)
-	# new_label.modulate = Color(0, 0, 0)
-	# new_label.text = "(" + str(array_position.x) + ", " + str(array_position.y) + ")"
-	# new_label.position = Vector2(-8, -8)
-	# piece.add_child(new_label)
-	add_child(piece)
-	piece.piece_selected.connect(_on_piece_selected)
-	piece.piece_deselected.connect(_on_piece_deselected)
-	piece.position = map_to_local(cell)
-	pieces_on_board[array_position.x][array_position.y] = piece
 
-func remove_piece(cell: Vector2i):
-	var array_position = _board_to_array_position(cell)
-	var piece = pieces_on_board[array_position.x][array_position.y]
-	piece.queue_free()
-	pieces_on_board[array_position.x][array_position.y] = null
-
-func _get_piece_at(cell: Vector2i) -> Piece:
-	var array_position = _board_to_array_position(cell)
-	return pieces_on_board[array_position.x][array_position.y]
-
-func _match_3_row(cell: Vector2i, piece_name: String) -> bool:
-	var array_position = _board_to_array_position(cell)
-
-	var matches = 1
-
-	var x_start = array_position.x
-	var y_start = array_position.y
-
-	for x in range(x_start + 1, pieces_on_board.size()):
-		if pieces_on_board[x][y_start] == null:
-			break
-
-		if pieces_on_board[x][y_start].piece_name == piece_name:
-			matches += 1
-		else:
-			break
-
-	for x in range(x_start - 1, 0, -1):
-		if pieces_on_board[x][y_start] == null:
-			break
-
-		if pieces_on_board[x][y_start].piece_name == piece_name:
-			matches += 1
-		else:
-			break
-
-	if matches >= 3:
-		return true
-
-	return false
-
-func _match_3_column(cell: Vector2i, piece_name: String) -> bool:
-	var array_position = _board_to_array_position(cell)
-
-	var matches = 1
-	
-	var x_start = array_position.x
-	var y_start = array_position.y
-
-	for y in range(y_start + 1, pieces_on_board[x_start].size()):
-		if pieces_on_board[x_start][y] == null:
-			break
-
-		if pieces_on_board[x_start][y].piece_name == piece_name:
-			matches += 1
-		else:
-			break
-
-	for y in range(y_start - 1, 0, -1):
-		if pieces_on_board[x_start][y] == null:
-			break
-		if pieces_on_board[x_start][y].piece_name == piece_name:
-			matches += 1
-		else:
-			break
-
-	if matches >= 3:
-		return true
-
-	return false
+func _slide_pieces_down() -> void:
+	for row in range(width):
+		for column in range(height - 1, -1, -1):
+			if board_pieces[row][column] == null:
+				for column_offset in range(column - 1, -1, -1):
+					if board_pieces[row][column_offset] != null:
+						board_pieces[row][column_offset].move(_grid_to_pixel(Vector2i(row, column)))
+						board_pieces[row][column] = board_pieces[row][column_offset]
+						board_pieces[row][column_offset] = null
+						break
 
 
-func _match_3_at(cell: Vector2i, piece_name: String) -> bool:
-	return _match_3_row(cell, piece_name) or _match_3_column(cell, piece_name)
-
-func _board_to_array_position(cell: Vector2i) -> Vector2i:
-	return cell - Vector2i(1, 1)
-
-func array_to_board_position(cell: Vector2i) -> Vector2i:
-	return cell + Vector2i(1, 1)
-
-
-func spawn_pieces():
-	for x in range(1, board_size.x - 1):
-		for y in range(1, board_size.y - 1):
-			var cell_location = Vector2i(x, y)
-			create_piece(cell_location)
-
-func _find_piece_on_array(piece: Piece) -> Vector2i:
-	for x in range(board_size.x - 2):
-		for y in range(board_size.y - 2):
-			if pieces_on_board[x][y] == piece:
-				return Vector2i(x, y)
-
-	return Vector2i(-1, -1)
-
-func _on_piece_selected(piece: Piece):
+func _on_piece_piece_selected(piece: Piece):
 	if selected_piece == null:
 		selected_piece = piece
+		selected_piece.selected = true
 		return
 
-	if can_move == false:
+	if selected_piece == piece:
 		selected_piece.selected = false
-		piece.selected = false
 		selected_piece = null
 		return
+	
+	var selected_piece_board_position = _pixe_to_grid(selected_piece.position)
+	var piece_board_position =_pixe_to_grid(piece.position)
 
-	var selected_piece_position = _find_piece_on_array(selected_piece)
-	var piece_position = _find_piece_on_array(piece)
+	if (selected_piece_board_position - piece_board_position).length() > 1:
+		selected_piece.selected = false
+		selected_piece = piece
+		selected_piece.selected = true
+		return
 
-	_swap_pieces(array_to_board_position(selected_piece_position), array_to_board_position(piece_position))
-
+	_swap_pieces(selected_piece_board_position, piece_board_position)
 	selected_piece.selected = false
 	piece.selected = false
 	selected_piece = null
 
-func _on_piece_deselected(_piece: Piece):
-	selected_piece = null
+func _swap_pieces(from: Vector2i, to: Vector2i) -> void:
+	_move_piece(from, to)
 
-	
-func _swap_pieces(cell_1: Vector2i, cell_2: Vector2i, already_swapped: bool = false):
-	can_move = false
+func _check_match(row: int, column: int, piece_type: String) -> bool:
+	if row > 1:
+		if (
+			board_pieces[row - 1][column] != null
+			and board_pieces[row - 2][column] != null
+			and board_pieces[row - 1][column].piece_type == piece_type
+			and board_pieces[row - 2][column].piece_type == piece_type):
+			return true
 
-	var array_position_1 = _board_to_array_position(cell_1)
-	var array_position_2 = _board_to_array_position(cell_2)
 
-	var piece_1: Piece = pieces_on_board[array_position_1.x][array_position_1.y]
-	var piece_2: Piece = pieces_on_board[array_position_2.x][array_position_2.y]
+	if column > 1:
+		if (
+			board_pieces[row][column - 1] != null
+			and board_pieces[row][column - 2] != null
+			and board_pieces[row][column - 1].piece_type == piece_type
+			and board_pieces[row][column - 2].piece_type == piece_type):
+			return true
 
-	pieces_on_board[array_position_1.x][array_position_1.y] = piece_2
-	pieces_on_board[array_position_2.x][array_position_2.y] = piece_1
+	return false
 
-	if tween != null:
-		tween.kill()
 
-	tween = get_tree().create_tween()
-	tween.tween_property(piece_1, "position", map_to_local(cell_2), 0.1).set_ease(Tween.EASE_IN_OUT)
-	tween.tween_property(piece_2, "position", map_to_local(cell_1), 0.1).set_ease(Tween.EASE_IN_OUT)
-	# tween.tween_property(piece_1.get_child(1), "text", "(" + str(array_position_2.x) + ", " + str(array_position_2.y) + ")", 0.1).set_ease(Tween.EASE_IN_OUT)
-	# tween.tween_property(piece_2.get_child(1), "text", "(" + str(array_position_1.x) + ", " + str(array_position_1.y) + ")", 0.1).set_ease(Tween.EASE_IN_OUT)
-	tween.parallel().play()
-	if !already_swapped:
-		tween.finished.connect(_on_tween_finished.bind(piece_1, piece_2))
+func _destroy_matched() -> void:
+	for row in range(width):
+		for column in range(height):
+			if board_pieces[row][column] == null:
+				continue
+			if not board_pieces[row][column].matched:
+				continue
+				
+			board_pieces[row][column].destroy_matched()
+			board_pieces[row][column] = null
 
-func _on_tween_finished(piece_1: Piece, piece_2: Piece):
 
-	var piece_1_position = _find_piece_on_array(piece_1)
-	var piece_2_position = _find_piece_on_array(piece_2)
-	if (!_match_3_at(array_to_board_position(piece_1_position), piece_1.piece_name) and !_match_3_at(array_to_board_position(piece_2_position), piece_2.piece_name)):
-		_swap_pieces(array_to_board_position(piece_1_position), array_to_board_position(piece_2_position), true)
-	
-	can_move = true
-	
+func _on_destroy_match_timer_timeout():
+	_destroy_matched()
+	slide_down_timer.start()
 
-func _ready():
-	randomize()
 
-	var cells: Array[Vector2i] = generate_cells()
-	pieces_on_board = generate_2d_array()
+func _find_and_set_matchs() -> void:
+	var match_found = false
+	for row in range(width):
+		for column in range(height):
+			if board_pieces[row][column] == null:
+				continue
 
-	spawn_pieces()
+			var piece = board_pieces[row][column]
+			var piece_type = piece.piece_type
 
-	set_cells_terrain_connect(cells, 0, 0)
+			if row > 1:
+				if (
+					board_pieces[row - 1][column] != null
+					and board_pieces[row - 2][column] != null
+					and board_pieces[row - 1][column].piece_type == piece_type
+					and board_pieces[row - 2][column].piece_type == piece_type):
+					board_pieces[row - 1][column].matched = true
+					board_pieces[row - 2][column].matched = true
+					piece.matched = true
+
+			if column > 1:
+				if (
+					board_pieces[row][column - 1] != null
+					and board_pieces[row][column - 2] != null
+					and board_pieces[row][column - 1].piece_type == piece_type
+					and board_pieces[row][column - 2].piece_type == piece_type):
+					board_pieces[row][column - 1].matched = true
+					board_pieces[row][column - 2].matched = true
+					piece.matched = true
+
+			if piece.matched:
+				match_found = true	
+			
+	if match_found:
+		destroy_match_timer.start()
+
+
+func _on_slide_down_timer_timeout() -> void:
+	_slide_pieces_down()
+	refill_timer.start()
+
+
+func _refill_pieces() -> void:
+	for row in width:
+		for column in height:
+			if board_pieces[row][column] == null:
+				var new_piece = _create_piece()
+				var spawn_board_position =  Vector2i(row, -1)
+				var target_board_position = Vector2i(row, column)
+				new_piece.position = _grid_to_pixel(spawn_board_position)
+				new_piece.move(_grid_to_pixel(target_board_position))
+				board_pieces[row][column] = new_piece
+
+
+func _on_refill_timer_timeout() -> void:
+	_refill_pieces()
+	_find_and_set_matchs()
--- pieces/scripts/piece.gd
+++ pieces/scripts/piece.gd
@@ -3,8 +3,18 @@ class_name Piece extends Sprite2D
 
 @export var piece_texture: Texture2D
 @export var selected_piece_texture: Texture2D
-@export var piece_name: String
-@export var selected: bool = false
+@export var piece_type: String
+@export var selected: bool = false : set = _set_selected
+
+var matched: bool = false : set = _set_matched
+
+func _set_matched(new_value: bool) -> void:
+	matched = new_value
+	modulate = Color(1, 1, 1, 0.5) if matched else Color(1, 1, 1, 1)
+
+func _set_selected(new_value: bool) -> void:
+	selected = new_value
+	texture = selected_piece_texture if selected else piece_texture
 
 @export var area_node: Area2D
 
@@ -12,6 +22,7 @@ signal piece_selected(piece: Piece)
 signal piece_deselected(piece: Piece)
 
 func _ready():
+	assert(piece_type != "", "Piece type must be set.")
 	texture = piece_texture
 	area_node.mouse_entered.connect(_on_piece_mouse_entered)
 	area_node.mouse_exited.connect(_on_piece_mouse_exited)
@@ -29,9 +40,13 @@ func _on_piece_mouse_button_pressed(_viewport: Node, event: InputEvent, _shape_i
 		if event.button_index == MOUSE_BUTTON_LEFT and event.pressed:
 			if selected:
 				selected = false
-				texture = piece_texture
 				piece_deselected.emit(self)
 			else:
 				selected = true
-				texture = selected_piece_texture
 				piece_selected.emit(self)
+
+func move(target_position: Vector2):
+	create_tween().tween_property(self, "position", target_position, 0.3).set_trans(Tween.TRANS_ELASTIC)
+
+func destroy_matched():
+	queue_free()
